8d55e95e1fb65d9b5a1f7a8c515ac63ad7642280
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-16 22:28:26 -0300

    dia 04 - 0506-Dynamic

---
+++ 05_Componentes/0506-Dynamic/src/components/AulaDownload.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Fala o download dos arquivos.</p>
+    <button>Download</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AulaDownload",
+  created() {
+    alert("created");
+  },
+  destroyed() {
+    alert("Destroyed");
+  },
+};
+</script>
+
+<style >
+</style>
