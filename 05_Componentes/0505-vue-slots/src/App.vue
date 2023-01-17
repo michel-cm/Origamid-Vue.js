61cfb2e936bb0fa4e8d1fa30809f6d81680b86d7
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-16 22:09:39 -0300

    dia 04 - 0505-vueSlots

---
+++ 05_Componentes/0505-vue-slots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <modal-principal>
+      <template #header><p>Esse é o header</p> </template>
+      <template #default="{ user }">
+        {{ user }}
+        <p>Esse é um teste</p>
+        <button>Comprar</button>
+      </template>
+      <template #footer> <p>Esse é o footer</p> </template>
+    </modal-principal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from "./components/ModalPrincipal.vue";
+
+export default {
+  name: "App",
+  components: {
+    ModalPrincipal,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 05_Componentes/0505-vue-slots/src/components/ModalPrincipal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal">
+    <slot name="header"></slot>
+    <h1>{{ user.nome }}</h1>
+    <slot :user="user"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalPrincipal",
+  data() {
+    return {
+      user: {
+        nome: "Michel",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
