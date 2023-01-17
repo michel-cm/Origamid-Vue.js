79e6296b449c923d4ab29890c72f2c4f25c365c8
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-17 14:02:43 -0300

    dia 05 - 0601-transition/exerc

---
+++ 06_Transitions/0601-transition-2-exerc/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <modalSign></modalSign>
+  </div>
+</template>
+
+<script>
+import ModalSign from "./components/ModalSign.vue";
+
+export default {
+  name: "App",
+  components: {
+    ModalSign,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 06_Transitions/0601-transition-2-exerc/src/components/ModalSign.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <button @click="modalAtivo = !modalAtivo">Cadastre-se</button>
+    <transition name="modal">
+      <div class="modal-wrapper" v-show="modalAtivo" @click="cliqueFora">
+        <div class="modal">
+          <h1>Cadastre-se</h1>
+          <form>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" />
+            <label for="senha">Senha</label>
+            <input type="senha" name="senha" id="senha" />
+            <input type="submit" value="Logar" />
+          </form>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalSign",
+  data() {
+    return {
+      modalAtivo: false,
+    };
+  },
+  methods: {
+    cliqueFora({ currentTarget, target }) {
+      if (currentTarget === target) this.modalAtivo = false;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+h1 {
+  margin: 0px 0px 20px 0px;
+}
+
+.modal-wrapper {
+  position: fixed;
+  background: rgba(0, 0, 0, 0.4);
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  overflow-y: scroll;
+}
+
+.modal {
+  background: #e5e5e5;
+  padding: 20px;
+  border-radius: 4px;
+  max-width: 400px;
+  margin: 120px auto;
+  z-index: 10;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input,
+button {
+  width: 100%;
+  border: 2px solid rgba(0, 0, 0, 0.2);
+  border-radius: 2px;
+  padding: 15px 10px;
+  font-size: 1rem;
+  margin-bottom: 20px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  cursor: pointer;
+}
+
+button {
+  display: block;
+  margin: 20px auto;
+  cursor: pointer;
+}
+
+input[type="submit"],
+button {
+  width: 200px;
+  background: #84e;
+  color: white;
+  text-transform: uppercase;
+  font-size: 1rem;
+}
+
+.modal-enter,
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-active,
+.modal-leave-to {
+  transition: opacity 0.3s;
+}
+
+.modal-enter-to .modal {
+  animation: slide 0.3s;
+}
+
+.modal-leave-to .modal {
+  animation: slide 0.3s reverse;
+}
+
+@keyframes slide {
+  from {
+    transform: translate3d(0, -40px, 0);
+  }
+  to {
+    transform: translate3d(0, 0px, 0);
+  }
+}
+</style>
