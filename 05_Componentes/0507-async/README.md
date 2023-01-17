0528f58197c2bb8138b2237cd4405a824ecf40bd
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-17 08:58:28 -0300

    dia 04 - 0507-async

---
+++ 05_Componentes/0507-async/README.md
@@ -0,0 +1,38 @@
+# vue-dynamic
+
+## Exercicio
+
+<!--
+  Crie 3 componentes:
+
+  1: Componente falando sobre a empresa
+  2: Componente com os serviços da empresa
+  3: Componente com um formulário de contato
+
+  Crie um menu, que ao clique ele mude os componentes
+  acima de forma dinâmica. Não destrua o componente
+ -->
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
---
+++ 05_Componentes/0507-async/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ul>
+      <li @click="abaAtiva = 'SobreEmpresa'">Sobre</li>
+      <li @click="abaAtiva = 'ServicosEmpresa'">Serviços</li>
+      <li @click="abaAtiva = 'ContatoEmpresa'">Contato</li>
+    </ul>
+    <keep-alive>
+      <component :is="abaAtiva"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import SobreEmpresa from "./components/SobreEmpresa.vue";
+
+export default {
+  name: "App",
+  components: {
+    SobreEmpresa,
+    ServicosEmpresa: () => import("./components/ServicosEmpresa.vue"),
+    ContatoEmpresa: () => import("./components/ContatoEmpresa.vue"),
+  },
+  data() {
+    return {
+      abaAtiva: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
