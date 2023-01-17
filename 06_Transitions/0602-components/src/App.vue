62b2d1c346112c8d61ebd0d3b01326acb132c59e
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-17 16:14:40 -0300

    dia 05 - 0602-components

---
+++ 06_Transitions/0602-components/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <transitionTeste></transitionTeste>
+  </div>
+</template>
+
+<script>
+import TransitionTeste from "./components/TransitionTeste.vue";
+
+export default {
+  name: "App",
+  components: {
+    TransitionTeste,
+  },
+};
+</script>
+
+<style>
+</style>
