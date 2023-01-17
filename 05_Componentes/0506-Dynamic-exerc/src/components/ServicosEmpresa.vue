4c3b1976da6a542ee296e4b75a5ed4b936673d3c
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-17 08:35:08 -0300

    dia 05 - 0506-dynamic/exerc

---
+++ 05_Componentes/0506-Dynamic-exerc/src/components/ServicosEmpresa.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Serviços</h1>
+    <ul>
+      <li>UX/UI</li>
+      <li>Front End</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ServicosEmpresa",
+};
+</script>
+
+<style >
+</style>
