4bd9262a5b9892e2e11c202ba6a0619631dbafcf
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-17 10:53:12 -0300

    dia 05 - Transitions/

---
+++ 06_Transitions/0601-transition-1/src/components/TransitionTeste.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="ativo = !ativo">Ativar</button>
+    <transition name="fade" @enter="enter" @leave="leave">
+      <h1 v-if="ativo">Serviços</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TransitionTeste",
+  data() {
+    return {
+      ativo: false,
+    };
+  },
+  methods: {
+    enter(elemento) {
+      console.log(elemento.classList.add("teste"));
+    },
+
+    leave() {
+      console.log("Elemento Sumiu");
+    },
+  },
+};
+</script>
+
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
