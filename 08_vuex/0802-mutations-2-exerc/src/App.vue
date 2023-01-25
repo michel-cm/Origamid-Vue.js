9709a4fb9088c4e139712286656d178bac49c27d
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-25 14:38:27 -0300

    0802-exerc

---
+++ 08_vuex/0802-mutations-2-exerc/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <AlunoView />
+    <CursoView />
+  </div>
+</template>
+
+<script>
+import AlunoView from "@/components/AlunoView.vue";
+import CursoView from "@/components/CursoView.vue";
+
+export default {
+  name: "App",
+  components: {
+    AlunoView,
+    CursoView,
+  },
+};
+</script>
+
+<style></style>
