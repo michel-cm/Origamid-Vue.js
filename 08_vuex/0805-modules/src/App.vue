49dfac9ce80e4866afaa7ca033971d1554739865
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-25 15:55:49 -0300

    0805-modules

---
+++ 08_vuex/0805-modules/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="livro in $store.state.livros" :key="livro.nome">
+        <p>{{ livro.nome }}</p>
+      </li>
+    </ul>
+    <h2>Livros lidos</h2>
+    <ul>
+      <li v-for="livro in livrosLidos(true)" :key="livro.nome">
+        <p>{{ livro.nome }}</p>
+      </li>
+    </ul>
+    {{ $store.state.acao.acao }}
+    <AlunoView />
+    <CursoView />
+  </div>
+</template>
+
+<script>
+import AlunoView from "@/components/AlunoView.vue";
+import CursoView from "@/components/CursoView.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "App",
+  components: {
+    AlunoView,
+    CursoView,
+  },
+  computed: {
+    ...mapGetters(["livrosLidos"]),
+  },
+  created() {
+    this.$store.dispatch("acao/puxarAcao");
+  },
+};
+</script>
+
+<style></style>
