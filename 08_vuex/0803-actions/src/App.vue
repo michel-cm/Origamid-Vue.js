f7f01a5dfbea9328c22e0eefd10e09f5972ad6e8
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-25 15:01:23 -0300

    0803-actions

---
+++ 08_vuex/0803-actions/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{ $store.state.acao }}
+    <AlunoView />
+    <CursoView />
+  </div>
+</template>
+
+<script>
+import AlunoView from "@/components/AlunoView.vue";
+import CursoView from "@/components/CursoView.vue";
+
+export default {
+  name: "App",
+  components: {
+    AlunoView,
+    CursoView,
+  },
+  created() {
+    this.$store.dispatch("puxarAcao");
+  },
+};
+</script>
+
+<style></style>
---
+++ 08_vuex/0803-actions/src/components/CursoView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Curso de JS</p>
+    <AulasView />
+  </div>
+</template>
+
+<script>
+import AulasView from "@/components/AulasView.vue";
+
+export default {
+  components: {
+    AulasView,
+  },
+};
+</script>
